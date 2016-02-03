5a83c81ba21aa352181ae818ff914a2316dfddfc
rainnerlins@gmail.com <rainnerlins@gmail.com>
2016-02-03 16:13:37 -0500

    Added Exception class namepace

---
+++ src/Biscuit/Exception/DependencyException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Refers to an object dependency not being defined within a method.
+ *
+ * @package    Biscuit PHP Framework
+ * @author     Rainner Lins <http://rainnerlins.com/>
+ * @copyright  (c) All Rights Reserved
+ * @license    See included LICENSE file
+ */
+namespace Biscuit\Exception;
+
+class DependencyException extends \Exception {
+
+    // ...
+}
