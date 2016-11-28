33b7f442db19073ae49626077f99d06ac74a993d
Rainner Lins <rainnerlins@gmail.com>
2016-11-28 17:52:17 -0500

    Fixed local class autoloader

--- autoloader.php
+++ autoloader.php
@@ -3,8 +3,9 @@
  * Autoloader
  */
 spl_autoload_register( function( $class="" ) {
-    $base  = rtrim( str_replace( "\\", "/", __DIR__ ), "/" ) ."/src";
+    $base  = rtrim( str_replace( "\\", "/", __DIR__ ), "/" );
     $class = rtrim( str_replace( "\\", "/", $class ), "/" );
+    $class = str_replace( "Biscuit/", "src/", $class );
     $file  = preg_replace( "/\/\/+/", "/", $base ."/". $class .".php" );
     is_file( $file ) && require_once( $file );
 }, true );
